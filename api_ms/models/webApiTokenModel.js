bd5c8b40da2cf0d054b35555cb8f0ea26986406a
Ajay Tiwari <54166862+ajay9211-2019@users.noreply.github.com>
2021-02-24 22:19:04 +0530

    Delete api_ms directory

--- api_ms/models/webApiTokenModel.js
+++
@@ -1,5 +0,0 @@
-var baseModel   = require( appRoot+'/models/baseModel' );
-
-exports.validateApiToken = (token) => {
-    return baseModel.read(`select * from web_api_tokens WHERE value = "${token}"`);
-};
